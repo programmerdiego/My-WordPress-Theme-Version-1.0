2549db58d4b64719fc6876011f68466f5a564fa1
Diego Sanchez <programmerdiego@gmail.com>
2017-06-06 23:50:18 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,11 @@
+/*
+Theme Name: Diego Sanchez Theme 
+Author: Diego Sanchez
+Author URL: http://www.diegosanchez.info
+Version: 1.0
+*/
+
+
 <footer class="site-footer">
 	<p><?php bloginfo('name'); ?> &copy; <?php echo date('Y');?></p> <!-- footer name,copyright and year -->
 
